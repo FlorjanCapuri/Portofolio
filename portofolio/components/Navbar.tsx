067aa205617abbe0a027c93a79b730c5c5da09a5
FlorjanCapuri <florjancapuri@gmail.com>
2023-05-01 15:52:49 +0300

    Fixed Layout added logo

--- portofolio/components/Navbar.tsx
+++ portofolio/components/Navbar.tsx
@@ -1,17 +1,34 @@
 import React from "react";
 
-const navLinks = [
+const navLinks1 = [
   { id: "Main", title: "Main" },
   { id: "About", title: "About" },
   { id: "Experience", title: "Experience" },
-  { id: "Contact", title: "Contact" },
 ];
+
+const navLinks2 = [{ id: "Contact", title: "Contact" }];
 
 const Navbar = () => {
   return (
-    <div className="bg-background text-white py-5">
-      <ul className="flex gap-10 justify-center list-none">
-        {navLinks.map((link) => (
+    <div className="bg-background text-white py-[28px] px-[20%] flex justify-between items-center gap-[32px]">
+      <ul className="flex items-center justify-center gap-10">
+        {navLinks1.map((link) => (
+          <li>
+            <a href={`#${link.id}`}>{link.title}</a>
+          </li>
+        ))}
+      </ul>
+      <a>
+        <img className="w-[190px] h-[43px]" src="/logo.png"></img>
+      </a>
+      <ul className="flex items-center justify-center gap-10">
+        <li>
+          <a href="">GitHub</a>
+        </li>
+        <li>
+          <a href="">Blog</a>
+        </li>
+        {navLinks2.map((link) => (
           <li>
             <a href={`#${link.id}`}>{link.title}</a>
           </li>
